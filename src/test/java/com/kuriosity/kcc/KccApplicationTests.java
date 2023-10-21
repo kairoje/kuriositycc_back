619e5c26237291fb8214dcc9272c67273ead146b
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-21 13:26:08 -0700

    inital commit

---
+++ src/test/java/com/kuriosity/kcc/KccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuriosity.kcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
