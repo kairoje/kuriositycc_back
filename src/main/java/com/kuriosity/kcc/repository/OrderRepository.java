6b764209fd1586afe116fe72988affbb266e3c3d
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-21 14:42:25 -0700

    initialize order repository

---
+++ src/main/java/com/kuriosity/kcc/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.kuriosity.kcc.repository;
+
+import com.kuriosity.kcc.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
