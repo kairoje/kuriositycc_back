95e5ff0b8ea1f7276c1667d02ecdd875e4a63c11
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-21 13:54:11 -0700

    models created and id column initiated in user

---
+++ src/main/java/com/kuriosity/kcc/model/Product.java
@@ -0,0 +1,4 @@
+package com.kuriosity.kcc.model;
+
+public class Product {
+}
---
+++ src/main/java/com/kuriosity/kcc/model/User.java
@@ -0,0 +1,13 @@
+package com.kuriosity.kcc.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
