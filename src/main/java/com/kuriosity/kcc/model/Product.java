e6b46f19f07f0b39e5d2cc7391547c0025ca4626
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-21 14:19:33 -0700

    create product getters and setters

--- src/main/java/com/kuriosity/kcc/model/Product.java
+++ src/main/java/com/kuriosity/kcc/model/Product.java
@@ -1,6 +1,6 @@
 package com.kuriosity.kcc.model;
 
-import javax.persistence.*
+import javax.persistence.*;
 
 @Entity
 @Table(name = "products")
@@ -31,4 +31,46 @@ public class Product {
 
     public Product() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+
 }
