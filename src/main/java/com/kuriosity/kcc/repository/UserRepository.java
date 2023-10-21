ce487c7976ff22f06eed9d7d47e03ab8d4c608ac
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-21 14:39:02 -0700

    initialize user repository

---
+++ src/main/java/com/kuriosity/kcc/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.kuriosity.kcc.repository;
+
+import com.kuriosity.kcc.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
