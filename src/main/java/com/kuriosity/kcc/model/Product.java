582bf907708410b665449e7eab98ae2932fb30f3
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-21 14:14:22 -0700

    create product id column

--- src/main/java/com/kuriosity/kcc/model/Product.java
+++ src/main/java/com/kuriosity/kcc/model/Product.java
@@ -1,4 +1,13 @@
 package com.kuriosity.kcc.model;
 
+import javax.persistence.*
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 }
