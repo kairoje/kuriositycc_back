a564e24c87e60a9f9447f264680bf7edaccc57c2
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-21 14:26:44 -0700

    establish order and product relationship in order model

--- src/main/java/com/kuriosity/kcc/model/Order.java
+++ src/main/java/com/kuriosity/kcc/model/Order.java
@@ -1,6 +1,7 @@
 package com.kuriosity.kcc.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -11,4 +12,14 @@ public class Order {
 
     @ManyToOne
     private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_product",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+
 }
