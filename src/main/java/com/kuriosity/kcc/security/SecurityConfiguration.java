2b3a806fd48f50375a097adcfc9bb0c444466b1a
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-21 16:05:01 -0700

    configure method implemented

--- src/main/java/com/kuriosity/kcc/security/SecurityConfiguration.java
+++ src/main/java/com/kuriosity/kcc/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.kuriosity.kcc.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,5 +19,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
 
 }
