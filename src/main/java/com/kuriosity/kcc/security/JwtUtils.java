9476a75553ecf50caf1f57dffe96d4398c24616c
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-21 16:26:44 -0700

    creation of jwt secret and expiration + implementation into jwtutils

---
+++ src/main/java/com/kuriosity/kcc/security/JwtUtils.java
@@ -0,0 +1,15 @@
+package com.kuriosity.kcc.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUtils {
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMs;
+
+}
