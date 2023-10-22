cf4de96568709a2960ffff950dea17344ec55f37
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-22 09:50:38 -0700

    login controller implementation

--- src/main/java/com/kuriosity/kcc/controller/LoginController.java
+++ src/main/java/com/kuriosity/kcc/controller/LoginController.java
@@ -1,4 +1,28 @@
 package com.kuriosity.kcc.controller;
 
+import com.kuriosity.kcc.model.User;
+import com.kuriosity.kcc.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class LoginController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public String login(@RequestBody User user) {
+        User existingUser = userRepository.findByUsername(user.getUsername());
+
+        if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
+            return "Login successful!";
+        } else {
+            return "Login failed. Please check your credentials";
+        }
+    }
 }
