cd0f8ee8c4fdf7e38a1f166a6e6de109e8054196
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-24 08:55:39 -0700

    inital creation of implementation order service class

---
+++ src/main/java/com/kuriosity/kcc/service/OrderServiceImpl.java
@@ -0,0 +1,27 @@
+package com.kuriosity.kcc.service;
+
+import com.kuriosity.kcc.model.Order;
+
+public class OrderServiceImpl implements OrderService{
+
+
+    @Override
+    public Order createOrder(Order orderRequest) {
+        return null;
+    }
+
+    @Override
+    public Order getOrderById(Long orderId) {
+        return null;
+    }
+
+    @Override
+    public Order updateOrder(Long orderId, Order updatedOrder) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(Long orderId) {
+
+    }
+}
