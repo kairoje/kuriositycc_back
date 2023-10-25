05eddcfb275ac21344977b6841f3720ba3f26e3c
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-24 21:38:08 -0700

    refactor with lombok dependency

--- src/main/java/com/kuriosity/kcc/model/Order.java
+++ src/main/java/com/kuriosity/kcc/model/Order.java
@@ -3,68 +3,29 @@ package com.kuriosity.kcc.model;
 import javax.persistence.*;
 import java.util.Date;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.DATE)
     @Column
     private Date date;
+
     @Column
     private Double total;
+
     @Column
     private String status;
-
-    public Order() {
-    }
-
-    public Order(Long id, Date date, Double total, String status) {
-        this.id = id;
-        this.date = date;
-        this.total = total;
-        this.status = status;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        this.total = total;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", date=" + date +
-                ", total=" + total +
-                ", status='" + status + '\'' +
-                '}';
-    }
 }
