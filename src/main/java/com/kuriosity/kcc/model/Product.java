c2bd35831058fe2805cf494ef310ba631af41077
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-24 21:40:50 -0700

    refactor product with lombok dependency

--- src/main/java/com/kuriosity/kcc/model/Product.java
+++ src/main/java/com/kuriosity/kcc/model/Product.java
@@ -1,7 +1,14 @@
 package com.kuriosity.kcc.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "products")
 public class Product {
@@ -21,65 +28,4 @@ public class Product {
     @Column
     private Integer stock;
 
-    public Product(Long id, String name, Double price, String description, Integer stock) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.stock = stock;
-    }
-
-    public Product() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                ", description='" + description + '\'' +
-                ", stock=" + stock +
-                '}';
-    }
 }
