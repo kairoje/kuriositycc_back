8b9f4fd9f61b4e1d63e54b72c7de0a47b885cd01
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-26 07:46:25 -0700

    refactor of user service to handle authentication

--- src/main/java/com/kuriosity/kcc/service/UserService.java
+++ src/main/java/com/kuriosity/kcc/service/UserService.java
@@ -5,28 +5,60 @@ import com.kuriosity.kcc.exception.InformationNotFound;
 import com.kuriosity.kcc.model.Order;
 import com.kuriosity.kcc.model.User;
 import com.kuriosity.kcc.repository.UserRepository;
+import com.kuriosity.kcc.security.AuthUserDetails;
+import com.kuriosity.kcc.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    public User register(User user) {
-        return userRepository.save(user);
+    private final PasswordEncoder passwordEncoder;
+
+    private final JWTUtils jwtUtils;
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils,
+                       @Lazy AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
     }
 
-    public User login(String username, String password) {
-        User user = userRepository.findByUsername(username);
-
-        if (user != null && user.getPassword().equals(password)) {
-            return user;
+    public User register(User user) {
+        if (!userRepository.existsByUsername(user.getUsername())){
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            return userRepository.save(user);
         } else {
-            throw new InformationAlreadyExists("User with username " + username + " already exists");
+            throw new InformationAlreadyExists("User with email address " + user.getUsername() + " already exists");
+        }
+    }
+
+    public Optional<String> login(String username, String password) {
+        UsernamePasswordAuthenticationToken authenticationToken = new
+                UsernamePasswordAuthenticationToken(username, password);
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            AuthUserDetails authUserDetails = (AuthUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(authUserDetails));
+        } catch (Exception e) {
+            return Optional.empty();
         }
     }
 
