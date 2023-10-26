b123575574cd5ec0fd1ff5d15b29706197143de6
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-25 21:05:22 -0700

    generateJwtToken() method created

--- src/main/java/com/kuriosity/kcc/security/JWTUtils.java
+++ src/main/java/com/kuriosity/kcc/security/JWTUtils.java
@@ -3,6 +3,7 @@ package com.kuriosity.kcc.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 @Service
@@ -16,5 +17,12 @@ public class JWTUtils {
     @Value("${jwt-expiration-ms}")
     private int jwtExpirationMs;
 
-    public String generateJwtToken(AuthUserDetails)
+    public String generateJwtToken(AuthUserDetails authUserDetails) {
+        return Jwts.builder()
+                .setSubject(authUserDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
 }
