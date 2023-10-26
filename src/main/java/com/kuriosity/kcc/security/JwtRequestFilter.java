ad59fd04521bb5f904383c99df7607ee8ecf7e7c
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-25 21:32:50 -0700

    doFilterInternal() method implementation

--- src/main/java/com/kuriosity/kcc/security/JwtRequestFilter.java
+++ src/main/java/com/kuriosity/kcc/security/JwtRequestFilter.java
@@ -1,8 +1,17 @@
 package com.kuriosity.kcc.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
@@ -16,5 +25,26 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     @Autowired
     public void setAuthUserDetailsService(AuthUserDetailsService authUserDetailsService) {
         this.authUserDetailsService = authUserDetailsService;
+    }
+
+    @Autowired
+    public setJwtUtils(JWTUtils jwtUtils) { this.jwtUtils = jwtUtils; }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+
+            if (jwt != null && jwtUtils.validateJwt(jwt)) {
+                String username = jwtUtils.getUsernameFromJwt(jwt);
+                UserDetails userDetails = this.authUserDetailsService.loadUsername(username);
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        } catch (Exception e) {
+            logger.info("Cannot set user authentication token");
+        }
+        filterChain.doFilter(request, response);
     }
 }
